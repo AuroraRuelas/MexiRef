e18e87cddef1f4eb07be64f247cb257bfb1b791e
AuroraRuelas <karyna.aurora@gmail.com>
2018-05-29 09:39:31 +0200

    Initial Commit

---
+++ MexiRef/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MexiRef
+//
+//  Created by KARYNA AURORA RUELAS GONZALEZ on 29/5/18.
+//  Copyright © 2018 AURORA RUELAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
