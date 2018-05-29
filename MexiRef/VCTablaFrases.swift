cf33f7e390b00f3311aac239b02cea491fb9e1bb
AuroraRuelas <karyna.aurora@gmail.com>
2018-05-29 12:47:55 +0200

    comenzando la tabla de frases

---
+++ MexiRef/VCTablaFrases.swift
@@ -0,0 +1,37 @@
+//
+//  VCTablaFrases.swift
+//  MexiRef
+//
+//  Created by KARYNA AURORA RUELAS GONZALEZ on 29/5/18.
+//  Copyright © 2018 AURORA RUELAS. All rights reserved.
+//
+
+import UIKit
+
+class VCTablaFrases: UIViewController {
+    
+    @IBOutlet var tablaFrases:UITableView?
+//    var arFrases[] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
