2f79f8297a5e1710489dd4d27a4fe3bad52381bc
AuroraRuelas <karyna.aurora@gmail.com>
2018-06-12 12:48:50 +0200

    acabo clase, comiezo de creacion de clases colecciones

---
+++ MexiRef/COLCO.swift
@@ -0,0 +1,29 @@
+//
+//  COLCO.swift
+//  MexiRef
+//
+//  Created by KARYNA AURORA RUELAS GONZALEZ on 12/6/18.
+//  Copyright © 2018 AURORA RUELAS. All rights reserved.
+//
+
+import UIKit
+
+class COLCO: NSObject {
+    let IDNOMBRE = "nombre"
+    let IDIMAGEN = "imagen"
+    var sID:String?
+    var sNombre:String?
+    var iImagen:String?
+    
+    
+    func setMap(valores:[String:Any]) {
+        sNombre = valores[IDNOMBRE] as! String
+        iImagen = valores[IDIMAGEN] as! String
+    }
+    func getMap() -> [String:Any] {
+        return[
+            IDNOMBRE: sNombre as Any,
+            IDIMAGEN: iImagen as Any
+        ]
+    }
+}
